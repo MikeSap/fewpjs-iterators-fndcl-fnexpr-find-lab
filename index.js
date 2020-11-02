aec32e718b210fbe2861c7a18bfddfc00564a59f
mike <mikesapienzaaudio@gmail.com>
2020-11-02 10:10:35 -0600

    commit tree f4685425f201ba0dd069e1704ea9dca7ed47adf7

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let win = array.find(superbowl => superbowl['result'] === 'W')
+  if (win !== undefined){
+    return win['year']
+  }
+}
